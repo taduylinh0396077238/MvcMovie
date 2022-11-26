7544d530c6ba15613d72c6511eb896fa6a0b5451
Duy Linh <91305016+taduylinh0396077238@users.noreply.github.com>
2022-11-26 08:26:59 +0700

    mvc

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -9,9 +9,12 @@ namespace MvcMovie.Controllers
         {
             return View();
         }
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is:{numTimes}");
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+
         }
     }
 }
