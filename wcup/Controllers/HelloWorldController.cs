53153b164e8108d00d0fedbbf3c4d7881f1a8fa2
Duy Linh <91305016+taduylinh0396077238@users.noreply.github.com>
2022-11-26 10:40:44 +0700

    WCUP

---
+++ wcup/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wcup.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(int time, int numTimes = 1)
+        {
+            ViewData["Message"] = "Match time" + time;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+
+        }
+    }
+}
